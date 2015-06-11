c8c787ff0bcd8e7c16194ca4e9dd747489131409
ChunfangWang <wchunfang@gmail.com>
2015-06-10 23:12:35 -0400

    add trainGMM

--- SegSuperPixels/segment.cpp
+++ SegSuperPixels/segment.cpp
@@ -18,82 +18,230 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
 #include <cstdio>
 #include <cstdlib>
-#include "image.h"
-#include "misc.h"
-#include "pnmfile.h"
-#include "segment-image.h"
+#include <ctime>
+// #include "image.h"
+// #include "misc.h"
+// #include "pnmfile.h"
+// #include "segment-image.h"
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <fstream>
 #include <cstring>
-#include <vl/fisher.h>
-#include <vl/gmm.h>
-#include <vl/slic.h>
+#include <algorithm>
+#include <iterator>
+
+extern "C" {
+  #include "vl/generic.h"
+  #include "vl/slic.h"
+  #include "vl/fisher.h"
+  #include "vl/gmm.h"
+}
 
 using namespace std;
 using namespace cv;
 
 
-int main(int argc, char **argv) {
-  if (argc != 6) {
-    fprintf(stderr, "usage: %s sigma k min input(ppm) output(ppm)\n", argv[0]);
-    return 1;
-  }
-  
-  float sigma = atof(argv[1]);
-  float k = atof(argv[2]);
-  int min_size = atoi(argv[3]);
-	
-  printf("loading input image.\n");
-  
-  //code chunfang wang added
-  Mat image1;
-  image1 = imread(argv[4], CV_LOAD_IMAGE_COLOR);
-    if(! image1.data )                              // Check for invalid input
+void printSegmentation(vl_uint32* &segmentation, Mat &mat, vl_size height, vl_size width
+  , vl_size channels)
+{
+  // Convert segmentation, and also store the superpixel regions
+    int** labels = new int*[mat.rows];
+    int num_superpixels = 0;
+    for (int i = 0; i < mat.rows; ++i) {
+        labels[i] = new int[mat.cols];
+
+        for (int j = 0; j < mat.cols; ++j) {
+            labels[i][j] = (int) segmentation[j + mat.cols*i];
+            cout<<labels[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    // Compute a contour image: this actually colors every border pixel
+    // red such that we get relatively thick contours.
+    int label = 0;
+    int labelTop = -1;
+    int labelBottom = -1;
+    int labelLeft = -1;
+    int labelRight = -1;
+
+    for (int i = 0; i < mat.rows; i++) {
+        for (int j = 0; j < mat.cols; j++) {
+
+            label = labels[i][j];
+
+            labelTop = label;
+            if (i > 0) {
+                labelTop = labels[i - 1][j];
+            }
+
+            labelBottom = label;
+            if (i < mat.rows - 1) {
+                labelBottom = labels[i + 1][j];
+            }
+
+            labelLeft = label;
+            if (j > 0) {
+                labelLeft = labels[i][j - 1];
+            }
+
+            labelRight = label;
+            if (j < mat.cols - 1) {
+                labelRight = labels[i][j + 1];
+            }
+
+            if (label != labelTop || label != labelBottom || label!= labelLeft || label != labelRight) {
+                mat.at<cv::Vec3b>(i, j)[0] = 0;
+                mat.at<cv::Vec3b>(i, j)[1] = 0;
+                mat.at<cv::Vec3b>(i, j)[2] = 255;
+            }
+        }
+    }
+
+    // Save the contour image.
+    cv::imwrite("SuperPixel_contour.jpg", mat);
+}
+
+float* IFVEncode(float* data, int dimensions, int numData, int numClusters)
+{
+    VlGMM* gmm = vl_gmm_new(VL_TYPE_FLOAT, dimensions, numClusters);
+    vl_gmm_cluster(gmm, data, numData);
+    float* enc = (float*)vl_malloc(sizeof(float) * 2 * dimensions * numClusters);
+    vl_fisher_encode(enc, VL_TYPE_FLOAT,  vl_gmm_get_means(gmm), dimensions, numClusters,
+    vl_gmm_get_covariances(gmm), vl_gmm_get_priors(gmm), data, numData, VL_FISHER_FLAG_IMPROVED);
+
+    //PCA_Whitening of the IFV feature vector
+    return enc;
+}
+
+//return a feature vector of size 8,576 values
+vector<float> featureVofSuperpixel(int x_min, int x_max, int y_min, int y_max, 
+    vector<KeyPoint> &keypoints, Mat &descriptors, Mat &mat_Lab)
+{
+    //find the index of keypoints which fall in this superpixel region
+    vector<KeyPoint>::iterator it;
+    int sp_n = 0; //the index id
+    vector<float> sp_descriptors;
+    for (it = keypoints.begin(); it < keypoints.end(); it++, sp_n++)
     {
-        cout <<  "Could not open or find the image" << endl ;
-        return -1;
-    }
-  //since images in OpenCV are always stored in the format of BGR, 
-  int width = image1.cols;
-  int height = image1.rows;
-  image<rgb> *im = new image<rgb>(width, height);
-  for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-        imRef(im, x, y).b = image1.at<Vec3b>(y,x)[0];
-        imRef(im, x, y).g = image1.at<Vec3b>(y,x)[1];
-        imRef(im, x, y).r = image1.at<Vec3b>(y,x)[2];
-      }  
+        if ((*it).pt.x <= x_max && (*it).pt.y <= y_max && 
+            (*it).pt.x >= x_min && (*it).pt.y >= y_min)
+        {
+            descriptors.row(i).copyTo(sp_descriptors);
+        }      
     }
 
-    // namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
-    // imshow( "Display window", image1 );                   // Show our image inside it.
-    // waitKey(0);                                          // Wait for a keystroke in the window
+    cout<<"Number of keypoints in this superpixel region: "<<sp_n<<endl;
+    //Use Improved Fisher Vector to encode the two feature channel.
+    //Feature Channel 1: SURF@64
+    float* enc_1 = IFVEncode((float*)sp_descriptors, descriptors.cols, sp_n, 64);
 
-//  image<rgb> *input = loadPPM(argv[4]);
- printf("processing\n");
- int num_ccs; 
- image<rgb> *seg = segment_image(im, sigma, k, min_size, &num_ccs); 
- 
- //save *seg into a jpeg image
- for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-        image1.at<Vec3b>(y,x)[0] = imRef(seg, x, y).b;
-        image1.at<Vec3b>(y,x)[1] = imRef(seg, x, y).g;
-        image1.at<Vec3b>(y,x)[2] = imRef(seg, x, y).r;
-      }  
-    }
+    //Feature Channel 2: Lab@64
+    vector<float> sp_Lab;
+    int count = 0;
+    for(int x = x_min; x <= x_max; x++)
+        for(int y = y_min; y <= y_max; y++, count++)
+        {
+            (float)mat_Lab.at<cv::Vec3b>(x, y).copyTo
+        }
 
-//save image into OpenCV
- imwrite(argv[5], image1 );
- printf("got %d components\n", num_ccs);
- printf("done! uff...thats hard work.\n");
+    cout<<"Number of points in this superpixel region: "<<i<<endl;
+    float* enc_2 = IFVEncode(sp_Lab.t(), 64);
+
+    return NULL;
+}
+
+int main(int argc, char **argv) {
 
 //try the vl_slic(im, regionSize, regularizer), where im is a single array
-regionSize = 10;
-regularizer = 10;
-segments = vl_slic(im->data, regionSize, reglarizer);
+clock_t t1,t2;
+t1=clock();
+cv::Mat mat = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+Mat mat_Lab = cvCreateMat(mat.rows, mat.cols, CV_8UC3);
+cvtColor(mat, mat_Lab, CV_BGR2Lab);
+imwrite("LabMat.jpg", mat_Lab);
 
+    // Convert image to one-dimensional array.
+    float* image = new float[mat.rows*mat.cols*mat.channels()];
+    for (int i = 0; i < mat.rows; ++i) {
+        for (int j = 0; j < mat.cols; ++j) {
+            // Assuming three channels ...
+            image[j + mat.cols*i + mat.cols*mat.rows*0] = mat.at<cv::Vec3b>(i, j)[0];
+            image[j + mat.cols*i + mat.cols*mat.rows*1] = mat.at<cv::Vec3b>(i, j)[1];
+            image[j + mat.cols*i + mat.cols*mat.rows*2] = mat.at<cv::Vec3b>(i, j)[2];
+        }
+    }
+
+// The algorithm will store the final segmentation in a one-dimensional array.
+    vl_uint32* segmentation = new vl_uint32[mat.rows*mat.cols];
+    vl_size height = mat.rows;
+    vl_size width = mat.cols;
+    vl_size channels = mat.channels();
+
+    // The region size defines the number of superpixels obtained.
+    // Regularization describes a trade-off between the color term and the
+    // spatial term.
+    vl_size region = 80;        
+    float regularization = 0.1;
+    vl_size minRegion = 200;
+
+    vl_slic_segment(segmentation, image, width, height, channels, region, regularization, minRegion);
+    printSegmentation(segmentation, mat, height, width, channels);
+
+//extract features both the SURF@64+COLOR@64 encoded using Improved Fisher Vectors and a GMM with 64 modes
+    //Detect the keypoints using SURF Detector
+    //first convert the RGB image into Grayscale
+    Mat grayMat = cvCreateMat(mat.rows, mat.cols, CV_8UC1);
+    cvtColor(mat, grayMat, CV_BGR2GRAY);
+    int minHessian = 400;
+    //surf(hessionThreshold, nOctaves, nOctaveLayers, extended(0:64, 1:128), upright)
+    SURF surf(minHessian, 4, 2, 0, 0);
+    Mat descriptors;
+    SurfFeatureDetector SurfDetector(minHessian);
+    vector<KeyPoint> keypoints;
+    surf(grayMat, cv::Mat(), keypoints, descriptors, false);
+    cout<<"Size of keypoints "<<keypoints.size()<<endl; 
+    cout<<"Descriptors size "<<descriptors.rows<<" "<<descriptors.cols<<endl;
+
+    //for each superpixels, compute two feature channels.
+//Step1: find the bounding box for each superpixel region based on segmentation
+    //map<label, <x_min, x_max, y_max, y_max>>
+    map<int, vector<int>> label_box;
+    for (int i = 0; i < mat.rows; ++i) {
+        for (int j = 0; j < mat.cols; ++j) {
+            int type = (int) segmentation[j + mat.cols*i];
+            if(!label_box[type].size()) {
+                label_box[type] = vector<int>(4);
+                label_box[type][0] = i;
+                label_box[type][1] = i;
+                label_box[type][2] = j;
+                label_box[type][3] = j;
+            } else {
+                label_box[type][0] = label_box[type][0] > i ? i : label_box[type][0]; 
+                label_box[type][1] = label_box[type][1] < i ? i : label_box[type][1]; 
+                label_box[type][2] = label_box[type][2] > j ? j : label_box[type][2]; 
+                label_box[type][3] = label_box[type][3] < j ? j : label_box[type][3]; 
+            }
+        }
+    }
+
+vector<float*> FV = featureVofSuperpixel(label_box[0][0], label_box[0][0], label_box[0][0], 
+label_box[0][0], keypoints, descriptors, mat_Lab);
+
+//encoded the two feature channels using the IFV+GMM with 64 modes
+//numClusters = 64, dimensions = 64, so the descriptor need to be transposed
+    Mat img_keypoints;
+    drawKeypoints( grayMat, keypoints, img_keypoints, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+    imwrite("SURF_keypoints.jpg", img_keypoints);
+
+
+    
+    t2=clock();
+    cout<<(((float)t2-(float)t1)/CLOCKS_PER_SEC)<<endl;
   return 0;
 }
---
+++ trainGMM/pca_opencv.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+using namespace std;
+
+///////////////////////
+// Functions
+static void read_imgList(const string& filename, vector<Mat>& images) {
+    std::ifstream file(filename.c_str(), ifstream::in);
+    if (!file) {
+        string error_message = "No valid input file was given, please check the given filename.";
+        CV_Error(Error::StsBadArg, error_message);
+    }
+    string line;
+    while (getline(file, line)) {
+        images.push_back(imread(line, 0));
+    }
+}
+
+static  Mat formatImagesForPCA(const vector<Mat> &data)
+{
+    Mat dst(static_cast<int>(data.size()), data[0].rows*data[0].cols, CV_32F);
+    for(unsigned int i = 0; i < data.size(); i++)
+    {
+        Mat image_row = data[i].clone().reshape(1,1);
+        Mat row_i = dst.row(i);
+        image_row.convertTo(row_i,CV_32F);
+    }
+    return dst;
+}
+
+static Mat toGrayscale(InputArray _src) {
+    Mat src = _src.getMat();
+    // only allow one channel
+    if(src.channels() != 1) {
+        CV_Error(Error::StsBadArg, "Only Matrices with one channel are supported");
+    }
+    // create and return normalized image
+    Mat dst;
+    cv::normalize(_src, dst, 0, 255, NORM_MINMAX, CV_8UC1);
+    return dst;
+}
+
+struct params
+{
+    Mat data;
+    int ch;
+    int rows;
+    PCA pca;
+    string winName;
+};
+
+static void onTrackbar(int pos, void* ptr)
+{
+    cout << "Retained Variance = " << pos << "%   ";
+    cout << "re-calculating PCA..." << std::flush;
+
+    double var = pos / 100.0;
+
+    struct params *p = (struct params *)ptr;
+
+    p->pca = PCA(p->data, cv::Mat(), PCA::DATA_AS_ROW, var);
+
+    Mat point = p->pca.project(p->data.row(0));
+    Mat reconstruction = p->pca.backProject(point);
+    reconstruction = reconstruction.reshape(p->ch, p->rows);
+    reconstruction = toGrayscale(reconstruction);
+
+    imshow(p->winName, reconstruction);
+    cout << "done!   # of principal components: " << p->pca.eigenvectors.rows << endl;
+}
+
+
+///////////////////////
+// Main
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        cout << "usage: " << argv[0] << " <image_list.txt>" << endl;
+        exit(1);
+    }
+
+    // Get the path to your CSV.
+    string imgList = string(argv[1]);
+
+    // vector to hold the images
+    vector<Mat> images;
+
+    // Read in the data. This can fail if not valid
+    try {
+        read_imgList(imgList, images);
+    } catch (cv::Exception& e) {
+        cerr << "Error opening file \"" << imgList << "\". Reason: " << e.msg << endl;
+        exit(1);
+    }
+
+    // Quit if there are not enough images for this demo.
+    if(images.size() <= 1) {
+        string error_message = "This demo needs at least 2 images to work. Please add more images to your data set!";
+        CV_Error(Error::StsError, error_message);
+    }
+
+    // Reshape and stack images into a rowMatrix
+    Mat data = formatImagesForPCA(images);
+
+    // perform PCA
+    PCA pca(data, cv::Mat(), PCA::DATA_AS_ROW, 0.95); // trackbar is initially set here, also this is a common value for retainedVariance
+
+    // Demonstration of the effect of retainedVariance on the first image
+    Mat point = pca.project(data.row(0)); // project into the eigenspace, thus the image becomes a "point"
+    Mat reconstruction = pca.backProject(point); // re-create the image from the "point"
+    reconstruction = reconstruction.reshape(images[0].channels(), images[0].rows); // reshape from a row vector into image shape
+    reconstruction = toGrayscale(reconstruction); // re-scale for displaying purposes
+
+    // init highgui window
+    string winName = "Reconstruction | press 'q' to quit";
+    namedWindow(winName, WINDOW_NORMAL);
+
+    // params struct to pass to the trackbar handler
+    params p;
+    p.data = data;
+    p.ch = images[0].channels();
+    p.rows = images[0].rows;
+    p.pca = pca;
+    p.winName = winName;
+
+    // create the tracbar
+    int pos = 95;
+    createTrackbar("Retained Variance (%)", winName, &pos, 100, onTrackbar, (void*)&p);
+
+    // display until user presses q
+    imshow(winName, reconstruction);
+
+    int key = 0;
+    while(key != 'q')
+        key = waitKey();
+
+   return 0;
+}
